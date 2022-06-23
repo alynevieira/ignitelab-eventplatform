07657e0862d9ebf5ba912d9dae14272af95770c4
alynevieira <alynealicevieira1@gmail.com>
2022-06-22 22:40:32 -0300

    Feature: Criando o arquivo de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-import { Event } from './pages/Event';
- 
+import { BrowserRouter } from 'react-router-dom';
+import { ApolloProvider } from '@apollo/client';
+import { client } from './lib/apollo';
+import { Router } from './Router';
+
 function App() {
   return (
-    <Event />
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+    </ApolloProvider>
   )
 }
 
