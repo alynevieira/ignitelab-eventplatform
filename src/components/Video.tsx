15d1d2e9a50bc417a55c23ecaa231ecef17263db
alynevieira <alynealicevieira1@gmail.com>
2022-06-22 22:41:25 -0300

    Feature: Populando as lessons na rota de eventos

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,31 +1,86 @@
 import { CaretRight, DiscordLogo, FileArrowDown, ImageSquare, Lightning } from "phosphor-react";
+import { DefaultUi, Player, Youtube } from "@vime/react";
 
-export function Video() {
+import '@vime/core/themes/default.css';
+import { gql, useQuery } from "@apollo/client";
+
+const GET_LESSON_BY_SLUG_QUERY = gql`
+  query GetLessonBySlug ($slug: String) {
+    lesson(where: {slug: $slug}) {
+      title
+      videoId
+      description
+      teacher {
+        bio
+        avatarURL
+        name
+      }
+    }
+  }
+`
+
+interface GetLessonBySlugResponse {
+  lesson: {
+    title: string,
+    videoId: string,
+    description: string,
+    teacher: {
+      bio: string,
+      avatarURL: string,
+      name: string
+    }
+  }
+}
+
+interface VideoProps {
+  lessonSlug: string;
+}
+
+export function Video(props: VideoProps) {
+  const { data } = useQuery<GetLessonBySlugResponse>(GET_LESSON_BY_SLUG_QUERY, {
+    variables: {
+      slug: props.lessonSlug
+    }
+  });
+
+  if(!data) {
+    return (
+      <div className="flex-1">
+        <p>Carregando...</p>
+      </div>
+    )
+  }
+
   return (
     <div className="flex-1">
       <div className="bg-black flex justify-center">
-        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video"></div>
+        <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+          <Player>
+            <Youtube videoId={data.lesson.videoId} />
+            <DefaultUi />
+          </Player>
+        </div>
       </div>
 
       <div className="p-8 max-w-[1100px] mx-auto">
         <div className="flex items-start gap-16">
           <div className="flex-1">
             <h1 className="text-2xl font-bold">
-              Aula 01 - Abertura do Ignite Lab
+            {data.lesson.title}
             </h1>
             <p className="mt-4 text-gray-200 leading-relaxed">
-              Nessa aula vamos dar inicio ao projeto criando uma estrutura base da aplicação utiizando taiwlind
+            {data.lesson.description}
             </p>
 
             <div className="flex items-center gap-4 mt-6">
               <img
                 className="h-16 w-16 rounded-full border-2 border-blue-500"
-                src="https://github.com/alynevieira.png"
-                alt="Alyne Vieira" />
+                src={data.lesson.teacher.avatarURL}
+                alt="Avatar" />
 
               <div className="leading-relaxed">
-                <strong className="font-bold text-2xl block">Alyne Vieira</strong>
-                <span className="text-gray-200 text-sm block">Front-End Developer</span>
+                <strong className="font-bold text-2xl block">{data.lesson.teacher.name}</strong>
+                <span className="text-gray-200 text-sm block">{data.lesson.teacher.bio}</span>
               </div>
             </div>
           </div>
